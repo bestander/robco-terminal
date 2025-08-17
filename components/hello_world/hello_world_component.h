aba9ae17aae4656197ca16dc116d080c897c0f3c
Konstantin Raev <konstantin.raev@pointsbet.com>
2025-08-17 10:26:48 -0400

    hello world draws on screen

--- components/hello_world/hello_world_component.h
+++ components/hello_world/hello_world_component.h
@@ -15,19 +15,19 @@ class HelloWorldComponent : public esphome::Component {
 public:
   void setup() override;
   void loop() override;
-// private:
-//   esp_lcd_panel_handle_t lcd_panel = nullptr;
-//   i2c_master_bus_handle_t my_bus = nullptr;
-//   esp_lcd_panel_io_handle_t touch_io_handle = nullptr;
-//   esp_lcd_touch_handle_t touch_handle = nullptr;
-//   lv_display_t *lvgl_disp = nullptr;
-//   lv_indev_t *lvgl_touch_indev = nullptr;
-//   esp_err_t app_lcd_init(esp_lcd_panel_handle_t *lp);
-//   esp_err_t app_touch_init(i2c_master_bus_handle_t *bus,
-//                           esp_lcd_panel_io_handle_t *tp_io,
-//                           esp_lcd_touch_handle_t *tp);
-//   esp_err_t app_lvgl_init(esp_lcd_panel_handle_t lp, esp_lcd_touch_handle_t tp,
-//                          lv_display_t **lv_disp, lv_indev_t **lv_touch_indev);
+private:
+  esp_lcd_panel_handle_t lcd_panel = nullptr;
+  i2c_master_bus_handle_t my_bus = nullptr;
+  esp_lcd_panel_io_handle_t touch_io_handle = nullptr;
+  esp_lcd_touch_handle_t touch_handle = nullptr;
+  lv_display_t *lvgl_disp = nullptr;
+  lv_indev_t *lvgl_touch_indev = nullptr;
+  esp_err_t app_lcd_init(esp_lcd_panel_handle_t *lp);
+  esp_err_t app_touch_init(i2c_master_bus_handle_t *bus,
+                          esp_lcd_panel_io_handle_t *tp_io,
+                          esp_lcd_touch_handle_t *tp);
+  esp_err_t app_lvgl_init(esp_lcd_panel_handle_t lp, esp_lcd_touch_handle_t tp,
+                         lv_display_t **lv_disp, lv_indev_t **lv_touch_indev);
 };
 
 
